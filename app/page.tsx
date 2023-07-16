0f5f480fe904acfacc91590e688a62e1462d9b0d
vykhy <vikehietoyhoshii@gmail.com>
2023-07-16 21:56:33 +0530

    project setup

---
+++ app/page.tsx
@@ -0,0 +1,11 @@
+function Home() {
+  return (
+    <section className="flex-start paddings mb-16 flex-col">
+      <h1>Categories</h1>
+      <h1>Posts</h1>
+      <h1>More</h1>
+    </section>
+  );
+}
+
+export default Home;
